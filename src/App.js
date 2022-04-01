60567feacff979f0f16b5c6eddcdf8ce8a19ebd6
vishalpwaman123 <vishalpwaman123@gmail.com>
2022-04-01 11:42:18 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import EmailTemplat from './components/EmailTemplat.js'
 export default function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<EmailTemplat />} />
-        </Routes>
-      </BrowserRouter> */}
       <EmailTemplat />
     </div>
   )
